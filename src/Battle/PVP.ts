50812919b6e810f1470392b0feb7b67ee9193660
Igormcf <igormaciel82@gmail.com>
2022-08-10 15:43:33 -0300

    Finaliza requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,25 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+class PVP extends Battle {
+  private _play1: Fighter;
+  private _play2: Fighter;
+
+  constructor(play1: Fighter, play2: Fighter) {
+    super(play1);
+
+    this._play1 = play1;
+    this._play2 = play2;
+  }
+
+  fight(): number {
+    if (this._play1.lifePoints > -1 && this._play2.lifePoints > -1) {
+      this._play1.attack(this._play2);
+      this._play2.attack(this._play1);
+    }
+
+    return super.fight();
+  }
+}
+
+export default PVP;
